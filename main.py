0da599a4efa22c3fda07056f28400c841a7545e1
KarineZhang <karine.zhang@hotmail.com>
2019-02-17 17:52:36 -0500

    Created put and get methods for book model

--- main.py
+++ main.py
@@ -16,22 +16,6 @@ def soen487_a1():
                     "student": {"id": "Your id#", "name": "Your name"}})
 
 
-@app.route("/user")
-def get_all_user():
-    user_list = models.User.query.all()
-    return jsonify([models.row2dict(user) for user in user_list])
-
-
-@app.route("/user/<user_id>")
-def get_user(user_id):
-    # id is a primary key, so we'll have max 1 result row
-    user = models.User.query.filter_by(id=user_id).first()
-    if user:
-        return jsonify(models.row2dict(user))
-    else:
-        return make_response(jsonify({"code": 404, "msg": "Cannot find this user id."}), 404)
-
-
 @app.route("/user", methods={"PUT"})
 def put_user():
     # get the name first, if no name then fail
@@ -65,7 +49,70 @@ def put_user():
         if app.config.get("DEBUG"):
             error += str(e)
         return make_response(jsonify({"code": 404, "msg": error}), 404)
-    return jsonify({"code": 200, "msg": "success"})
+    return jsonify({"code": 200, "msg": "Successfully added user."})
+
+
+@app.route("/user")
+def get_all_user():
+    user_list = models.User.query.all()
+    return jsonify([models.row2dict(user) for user in user_list])
+
+
+@app.route("/user/<user_id>")
+def get_user(user_id):
+    # id is a primary key, so we'll have max 1 result row
+    user = models.User.query.filter_by(id=user_id).first()
+    if user:
+        return jsonify(models.row2dict(user))
+    else:
+        return make_response(jsonify({"code": 404, "msg": "Cannot find this user id."}), 404)
+
+
+@app.route("/book", methods={"PUT"})
+def put_book():
+
+    book_code = request.form.get("book_code")
+    title = request.form.get("title")
+    author = request.form.get("author")
+    year = request.form.get("year")
+
+    if not book_code and not title and not author and not year:
+        return make_response(jsonify({"code": 403,
+                                      "msg": "Cannot put book. Missing mandatory fields."}), 403)
+    book_id = request.form.get("id")
+    if not book_id:
+        p = models.Book(book_code=book_code, title=title, author=author, year=year)
+    else:
+        p = models.Book(id=book_id, book_code=book_code, title=title, author=author, year=year)
+
+    models.db.session.add(p)
+    try:
+        models.db.session.commit()
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        error = "Cannot put book. "
+        print(app.config.get("DEBUG"))
+        if app.config.get("DEBUG"):
+            error += str(e)
+        return make_response(jsonify({"code": 404, "msg": error}), 404)
+    return jsonify({"code": 200, "msg": "Successfully added book."})
+
+
+@app.route("/book")
+def get_all_book():
+    book_list = models.Book.query.all()
+    return jsonify([models.row2dict(book) for book in book_list])
+
+
+@app.route("/book/<book_id>")
+def get_book(book_id):
+    # id is a primary key, so we'll have max 1 result row
+    user = models.Book.query.filter_by(id=book_id).first()
+    if user:
+        return jsonify(models.row2dict(user))
+    else:
+        return make_response(jsonify({"code": 404, "msg": "Cannot find this book id."}), 404)
+
+
 
 
 if __name__ == '__main__':
